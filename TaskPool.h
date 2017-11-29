608c6408bcfcf44aff5af1938e4213811018071f
Hyper0102 <luchikstreluchik@gmail.com>
2017-11-29 12:14:20 +0300

    Update TaskPool.h

--- TaskPool.h
+++ TaskPool.h
@@ -6,46 +6,76 @@ class TASKPOOL_FUNC
 {  private:
    
    typedef struct 
-   {  HANDLE Done;
-      HANDLE Active;
-      void *Param;
+   {  void *Param;
+      int Number;
+      TASKPOOL_FUNC<__TaskFunc, __NTASK> *TaskPool;
+      CRITICAL_SECTION Active;
    } TASKINFO;
 
-   HANDLE HndlDone[__NTASK];
    TASKINFO TaskInfo[__NTASK];
+
+   CRITICAL_SECTION Select;
+   HANDLE Wait;
+   int Number;
+   HANDLE Send;
+   HANDLE Done;
    HANDLE ThreadHndl[__NTASK];
+      
+   static DWORD WINAPI TaskThread(  LPVOID lpParameter)
+   {  TASKINFO *pInfo=(TASKINFO *) lpParameter;
+      TASKPOOL_FUNC<__TaskFunc, __NTASK> *pTaskPool=pInfo->TaskPool;
 
-
-   static DWORD WINAPI Thread(  LPVOID lpParameter)
-   {  TASKINFO *p=(TASKINFO *)lpParameter;
       while(1)
-      {  WaitForSingleObject(p->Active, INFINITE);
-         __TaskFunc(p->Param);
-         SetEvent(p->Done);
+      {  EnterCriticalSection(&pTaskPool->Select);
+         pTaskPool->Number=pInfo->Number;
+         //SetEvent(pTaskPool->Wait);
+         ReleaseSemaphore(pTaskPool->Wait,1,NULL);
+
+         WaitForSingleObject(pTaskPool->Send, INFINITE);
+         EnterCriticalSection(&pInfo->Active);
+         SetEvent(pTaskPool->Done);
+         LeaveCriticalSection(&pTaskPool->Select);
+         __TaskFunc(pInfo->Param);
+         LeaveCriticalSection(&pInfo->Active);
       }
+
       return 0;
    }
 
    public:
    TASKPOOL_FUNC()
-	{  for(int i=0; i<__NTASK; i++)
-      {  TaskInfo[i].Active=CreateEvent(NULL,false,false,NULL);
-         HndlDone[i]=CreateEvent(NULL,true,true,NULL);
-         TaskInfo[i].Done=HndlDone[i];
+	{  
+	   InitializeCriticalSection(&Select);
+      //Wait=CreateEvent(NULL,false,false,NULL);
+      Wait=CreateSemaphore(NULL,0, 10000, NULL);
+      Send=CreateEvent(NULL,false,false,NULL);
+      Done=CreateEvent(NULL,false,false,NULL);
+
+      for(int i=0; i<__NTASK; i++)
+      {  InitializeCriticalSection(&TaskInfo[i].Active);
       }
+      
+      for(int i=0; i<__NTASK; i++)
+      {  TaskInfo[i].TaskPool=this;
+      } 
+
+      for(int i=0; i<__NTASK; i++)
+      {  TaskInfo[i].Number=i;
+      } 
+      
       for(int i=0; i<__NTASK; i++)
       {   DWORD tid; 
-          ThreadHndl[i]=CreateThread(NULL, 0, Thread , &TaskInfo[i], 0, &tid);
+          ThreadHndl[i]=CreateThread(NULL, 0, TaskThread , &TaskInfo[i], 0, &tid);
       }
 	}
 
-   bool WaitForAnyReady(int *pNtask,  DWORD dwMilliseconds)
-   {  int stop=1;
-      DWORD n_res=WaitForMultipleObjects(__NTASK, HndlDone, false, dwMilliseconds);
-		if ((n_res >= WAIT_OBJECT_0) && (n_res < WAIT_OBJECT_0+__NTASK))
-		{  DWORD ntask=n_res - WAIT_OBJECT_0;
-         ResetEvent(HndlDone[n_res]);
-         *pNtask = n_res;
+   bool WaitForAnyReady(int *pNtask)
+   {  DWORD n_res=WaitForSingleObject(Wait, INFINITE);
+      
+		if (n_res == WAIT_OBJECT_0 )
+		{  if (pNtask!=NULL)
+         {  *pNtask = Number;
+         }
          return true;
 		}
 		else
@@ -53,81 +83,115 @@ class TASKPOOL_FUNC
 		}
    }
 
-   void WaitForAllComplete(DWORD dwMilliseconds)
-	{	WaitForMultipleObjects(__NTASK, HndlDone, true, dwMilliseconds); 
+   void WaitForAllComplete()
+	{	for(int i=0; i<__NTASK; i++)
+      {  EnterCriticalSection(&TaskInfo[i].Active);
+      }
+      for(int i=0; i<__NTASK; i++)
+      {  LeaveCriticalSection(&TaskInfo[i].Active);
+      }
 	}
 
-   void Start(int ntask, void *p)
-   {  TaskInfo[ntask].Param=p;
-      SetEvent(TaskInfo[ntask].Active);
+   void Start(void *p)
+   {  TaskInfo[Number].Param=p;
+      SetEvent(Send);
+      WaitForSingleObject(Done, INFINITE);
    }
 
 	~TASKPOOL_FUNC()
-	{  for(int i=0; i<__NTASK; i++)
+	{  
+      for(int i=0; i<__NTASK; i++)
 		{  CloseHandle(ThreadHndl[i]);
-         CloseHandle(TaskInfo[i].Done);
-         CloseHandle(TaskInfo[i].Active);
 		}
+
+      for(int i=0; i<__NTASK; i++)
+      {  DeleteCriticalSection(&TaskInfo[i].Active);
+      }
+
+      CloseHandle(Done);
+      CloseHandle(Send);
+      CloseHandle(Wait);
+      DeleteCriticalSection(&Select);
 	}
 };
 
 
-//-----------------------------------------------------------------------//
+//--------------------------------------//
 
 template <class _class_T, int __NTASK>
 class TASKPOOL_CLASS
-{  public:
+{  private:
    
    typedef struct 
-   {  HANDLE Done;
-      HANDLE Active;
-      void *Param;
-     _class_T Obj;  
+   {  void *Param;
+      int Number;
+      _class_T Obj;
+      TASKPOOL_CLASS<_class_T, __NTASK> *TaskPool;
+      CRITICAL_SECTION Active;
    } TASKINFO;
 
-   HANDLE HndlDone[__NTASK];
-   
-   HANDLE ThreadHndl[__NTASK];
-   
-   public:
    TASKINFO TaskInfo[__NTASK];
 
-   private:
+   CRITICAL_SECTION Select;
+   HANDLE Wait;
+   int Number;
+   HANDLE Send;
+   HANDLE Done;
 
-   static DWORD WINAPI Thread( LPVOID lpParameter)
-   {  TASKINFO *p=(TASKINFO *)lpParameter;
+   HANDLE ThreadHndl[__NTASK];
+      
+   static DWORD WINAPI TaskThread(  LPVOID lpParameter)
+   {  TASKINFO *pInfo=(TASKINFO *) lpParameter;
+      TASKPOOL_CLASS<_class_T, __NTASK> *pTaskPool=pInfo->TaskPool;
+
       while(1)
-      {  WaitForSingleObject(p->Active, INFINITE);
-         p->Obj.TaskProc(p->Param);
-         SetEvent(p->Done);
+      {  EnterCriticalSection(&pTaskPool->Select);
+         pTaskPool->Number=pInfo->Number;
+         SetEvent(pTaskPool->Wait);
+         WaitForSingleObject(pTaskPool->Send, INFINITE);
+         EnterCriticalSection(&pInfo->Active);
+         SetEvent(pTaskPool->Done);
+         LeaveCriticalSection(&pTaskPool->Select);
+         pInfo->Obj.TaskProc(pInfo->Param);
+         LeaveCriticalSection(&pInfo->Active);
       }
+
       return 0;
    }
 
    public:
    TASKPOOL_CLASS()
-	{  for(int i=0; i<__NTASK; i++)
-      {  TaskInfo[i].Active=CreateEvent(NULL,false,false,NULL);
-         HndlDone[i]=CreateEvent(NULL,true,true,NULL);
-         TaskInfo[i].Done=HndlDone[i];
+	{  
+	   InitializeCriticalSection(&Select);
+      Wait=CreateEvent(NULL,false,false,NULL);
+      Send=CreateEvent(NULL,false,false,NULL);
+      Done=CreateEvent(NULL,false,false,NULL);
+
+      for(int i=0; i<__NTASK; i++)
+      {  InitializeCriticalSection(&TaskInfo[i].Active);
       }
+      
+      for(int i=0; i<__NTASK; i++)
+      {  TaskInfo[i].TaskPool=this;
+      } 
+
+      for(int i=0; i<__NTASK; i++)
+      {  TaskInfo[i].Number=i;
+      } 
+      
       for(int i=0; i<__NTASK; i++)
       {   DWORD tid; 
-          ThreadHndl[i]=CreateThread(NULL, 0, Thread , &TaskInfo[i], 0, &tid);
-          if (ThreadHndl[i]==NULL)
-          {  int err=GetLastError();
-             printf("%s %d:CreateThread Error=%d i=%d\n",__FILE__,__LINE__, err, i);
-          }
+          ThreadHndl[i]=CreateThread(NULL, 0, TaskThread , &TaskInfo[i], 0, &tid);
       }
 	}
 
-   bool WaitForAnyReady(int *pNtask,  DWORD dwMilliseconds)
-   {  int stop2=1;
-      DWORD n_res=WaitForMultipleObjects(__NTASK, HndlDone, false, dwMilliseconds);
-		if ((n_res >= WAIT_OBJECT_0) && (n_res < WAIT_OBJECT_0+__NTASK))
-		{  DWORD ntask=n_res - WAIT_OBJECT_0;
-         ResetEvent(HndlDone[n_res]);
-         *pNtask = n_res;
+   bool WaitForAnyReady(int *pNtask)
+   {  DWORD n_res=WaitForSingleObject(Wait, INFINITE);
+      
+		if (n_res == WAIT_OBJECT_0 )
+		{  if (pNtask!=NULL)
+         {  *pNtask = Number;
+         }
          return true;
 		}
 		else
@@ -135,24 +199,37 @@ class TASKPOOL_CLASS
 		}
    }
 
-   void WaitForAllComplete(DWORD dwMilliseconds)
-	{	WaitForMultipleObjects(__NTASK, HndlDone, true, dwMilliseconds); 
+   void WaitForAllComplete()
+	{	for(int i=0; i<__NTASK; i++)
+      {  EnterCriticalSection(&TaskInfo[i].Active);
+      }
+      for(int i=0; i<__NTASK; i++)
+      {  LeaveCriticalSection(&TaskInfo[i].Active);
+      }
 	}
 
-   void Start(int ntask, void *p)
-   {  int stop=3;
-      TaskInfo[ntask].Param=p;
-      SetEvent(TaskInfo[ntask].Active);
+   void Start(void *p)
+   {  TaskInfo[Number].Param=p;
+      SetEvent(Send);
+      WaitForSingleObject(Done, INFINITE);
    }
 
 	~TASKPOOL_CLASS()
-	{  for(int i=0; i<__NTASK; i++)
+	{  
+      for(int i=0; i<__NTASK; i++)
 		{  CloseHandle(ThreadHndl[i]);
-         CloseHandle(TaskInfo[i].Done);
-         CloseHandle(TaskInfo[i].Active);
 		}
+
+      for(int i=0; i<__NTASK; i++)
+      {  DeleteCriticalSection(&TaskInfo[i].Active);
+      }
+
+      CloseHandle(Done);
+      CloseHandle(Send);
+      CloseHandle(Wait);
+      DeleteCriticalSection(&Select);
 	}
 };
 
-
 #endif
+
